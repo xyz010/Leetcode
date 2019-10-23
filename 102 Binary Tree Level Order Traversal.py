1b9df76c9d1e49fc8ec3ef48a30aefce83250f69
Andreas <32981058+xyz010@users.noreply.github.com>
2019-10-23 16:25:07 +0300

    Create 102 Binary Tree Level Order Traversal.py

---
+++ 102 Binary Tree Level Order Traversal.py
@@ -0,0 +1,87 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        
+        # base case
+        if root is None:
+            return []
+        # this is the iterative apporach should consider doing the recurrsive 
+        result,current = [], [root]
+        while current:
+            next_level, vals = [], []
+            for node in current:
+                vals.append(node.val)
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+            current = next_level
+            result.append(vals)
+        return result
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+def int2dArrayToString(input):
+    return json.dumps(input)
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+    lines = readlines()
+    while True:
+        try:
+            line = lines.next()
+            root = stringToTreeNode(line)
+            
+            ret = Solution().levelOrder(root)
+
+            out = int2dArrayToString(ret)
+            print out
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
